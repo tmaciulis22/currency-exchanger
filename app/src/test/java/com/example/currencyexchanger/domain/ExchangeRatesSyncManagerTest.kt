36f819efeac8b966a654c67131abee92814f8c54
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2022-12-21 16:24:42 +0200

    Cover ExchangeRatesSyncManager with tests

---
+++ app/src/test/java/com/example/currencyexchanger/domain/ExchangeRatesSyncManagerTest.kt
@@ -0,0 +1,30 @@
+package com.example.currencyexchanger.domain
+
+import com.example.currencyexchanger.data.repository.ExchangeRatesRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class ExchangeRatesSyncManagerTest {
+
+    @Test
+    fun testStart(): Unit = runBlocking {
+        val mockExchangeRatesRepository = mockk<ExchangeRatesRepository>()
+        coEvery { mockExchangeRatesRepository.updateExchangeRates() } returns Unit
+        coEvery { mockExchangeRatesRepository.getExchangeRates() } returns flow {  }
+
+        val manager = ExchangeRatesSyncManager(mockExchangeRatesRepository, refreshRate = 1L)
+
+        val job = launch {
+            manager.start()
+        }
+        delay(5L)
+        job.cancel()
+        coVerify(atLeast = 1) { mockExchangeRatesRepository.updateExchangeRates() }
+    }
+}
