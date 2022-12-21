41b42c9528a50b4aabcd013d16261952fb253bf7
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2022-12-21 21:16:14 +0200

    Update Config.kt

--- app/src/main/java/com/example/currencyexchanger/di/Config.kt
+++ app/src/main/java/com/example/currencyexchanger/di/Config.kt
@@ -6,5 +6,5 @@ object Config {
 
     const val BASE_URL = "https://api.apilayer.com/exchangerates_data/"
     const val HEADER_PARAM_API_KEY = "apikey"
-    const val API_KEY = "i1PMfVUbzMxWJlyZ8ONvqnjbdMZnbKYF"
+    const val API_KEY = "HSnVHQrBwWjL4ROv265XP81DY3KUpeXu"
 }
