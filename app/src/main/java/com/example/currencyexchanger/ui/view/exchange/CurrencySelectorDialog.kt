db32d28384f94718d1519e2e2b83d14c28051046
Tautvydas Mačiulis <maciulis.tautvydas22@gmail.com>
2022-12-21 20:03:01 +0200

    Update CurrencySelectorDialog.kt

--- app/src/main/java/com/example/currencyexchanger/ui/view/exchange/CurrencySelectorDialog.kt
+++ app/src/main/java/com/example/currencyexchanger/ui/view/exchange/CurrencySelectorDialog.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Icon
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.currencyexchanger.R
 
@@ -24,28 +26,37 @@ fun CurrencySelectorDialog(
     onDismissRequest: () -> Unit,
     onSelectedCurrency: (String) -> Unit,
 ) {
+    val lazyColumnHeightModifier = if (currencies.size > 5)
+        Modifier.fillMaxHeight(0.75f)
+    else
+        Modifier
+
     Dialog(onDismissRequest = onDismissRequest) {
         LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.75f)
+            modifier = lazyColumnHeightModifier
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(currencies, { it }) {
-                Row {
-                    OutlinedButton(
+                OutlinedButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(64.dp),
+                    onClick = {
+                        onSelectedCurrency(it)
+                        onDismissRequest()
+                    }
+                ) {
+                    Row(
                         modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            onSelectedCurrency(it)
-                            onDismissRequest()
-                        }
+                        horizontalArrangement = Arrangement.SpaceBetween
                     ) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Text(text = it)
-                            if (selectedCurrency == it)
-                                Icon(Icons.Filled.Check, stringResource(id = R.string.currency_picker_check_icon_description))
+                        Text(text = it)
+                        if (selectedCurrency == it) {
+                            Icon(
+                                Icons.Filled.Check,
+                                stringResource(id = R.string.currency_picker_check_icon_description)
+                            )
                         }
                     }
                 }
